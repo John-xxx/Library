6a5fbfbd712b24a9ed483d874ca4fbe9be1bb2e1
Liux <lx0758@qq.com>
2017-12-18 09:43:49 +0800

    增加状态栏工具方法

--- util/src/main/java/com/liux/util/ScreenUtil.java
+++ util/src/main/java/com/liux/util/ScreenUtil.java
@@ -9,7 +9,11 @@ import android.os.Build;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * Created by Liux on 2016/11/25.
@@ -102,6 +106,30 @@ public class ScreenUtil {
     }
 
     /**
+     *  设置系统状态栏图标和字体配色模式
+     * @param darkmode 是否深色模式
+     * @return 成功执行返回true
+     */
+    public static boolean setStatusBarMode(Activity activity, boolean darkmode) {
+        if (setMiuiStatusBarMode(activity, darkmode)) return true;
+        if (setMeizuStatusBarMode(activity, darkmode)) return true;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            View decor = activity.getWindow().getDecorView();
+            int ui = decor.getSystemUiVisibility();
+            if (darkmode) {
+                ui |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            } else {
+                ui &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            }
+            decor.setSystemUiVisibility(ui);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
      * 获取当前屏幕截图，包含状态栏
      * @param activity
      * @return
@@ -150,5 +178,61 @@ public class ScreenUtil {
         Display display = ((WindowManager) context.getApplicationContext()
                 .getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
         return display;
+    }
+
+    /**
+     * 设置 MIUIV6+ 系统状态栏图标和字体配色模式 <br>
+     * @param activity 需要设置的Activity
+     * @param darkmode 是否把状态栏字体及图标颜色设置为深色
+     * @return 成功执行返回true
+     */
+    private static boolean setMiuiStatusBarMode(Activity activity, boolean darkmode) {
+        // 开发版 7.7.13 以前版本
+        try {
+            int darkModeFlag = 0;
+            Class<? extends Window> clazz = activity.getWindow().getClass();
+            Class<?> layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+            Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
+            darkModeFlag = field.getInt(layoutParams);
+            Method extraFlagField = clazz.getMethod(darkmode ? "addExtraFlags" : "clearExtraFlags", int.class);
+            extraFlagField.invoke(activity.getWindow(), darkModeFlag);
+        } catch (Exception e) {}
+
+        // 开发版 7.7.13 及以后版本(和原生一致)
+        // http://www.miui.com/thread-8946673-1-1.html
+        //try {
+        //    Window window = activity.getWindow();
+        //    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        //    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        //} catch (Exception e) {}
+        return false;
+    }
+
+    /**
+     * 设置 Flyme 系统状态栏图标和字体配色模式 <br>
+     * @param activity 需要设置的Activity
+     * @param darkmode 是否把状态栏字体及图标颜色设置为深色
+     * @return 成功执行返回true
+     */
+    private static boolean setMeizuStatusBarMode(Activity activity, boolean darkmode) {
+        try {
+            WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
+            Field darkFlag = WindowManager.LayoutParams.class.getDeclaredField("MEIZU_FLAG_DARK_STATUS_BAR_ICON");
+            Field meizuFlags = WindowManager.LayoutParams.class.getDeclaredField("meizuFlags");
+            darkFlag.setAccessible(true);
+            meizuFlags.setAccessible(true);
+            int bit = darkFlag.getInt(null);
+            int value = meizuFlags.getInt(lp);
+            if (darkmode) {
+                value |= bit;
+            } else {
+                value &= ~bit;
+            }
+            meizuFlags.setInt(lp, value);
+            activity.getWindow().setAttributes(lp);
+            return true;
+        } catch (Exception e) {}
+        return false;
     }
 }
