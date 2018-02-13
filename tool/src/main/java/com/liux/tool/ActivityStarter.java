306c0d233e0fd365b880785748931d56a1b9a8a9
Liux <lx0758@qq.com>
2018-02-13 18:00:54 +0800

    修复一波因使用高版本SDK引起的BUG

--- tool/src/main/java/com/liux/tool/ActivityStarter.java
+++ tool/src/main/java/com/liux/tool/ActivityStarter.java
@@ -3,7 +3,6 @@ package com.liux.tool;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
 import android.util.SparseArray;
 
 /**
@@ -17,11 +16,12 @@ public class ActivityStarter {
 
     public static void startActivityForResult(Activity activity, Intent intent, Callback callback) {
         synchronized (activity) {
-            if (activity instanceof FragmentActivity) {
-                innerSupportFragmentForResult((FragmentActivity) activity, intent, callback);
-            } else {
-                innerFragmentForResult(activity, intent, callback);
-            }
+//            if (activity instanceof android.support.v4.app.FragmentActivity) {
+//                innerSupportFragmentForResult((android.support.v4.app.FragmentActivity) activity, intent, callback);
+//            } else {
+//                innerFragmentForResult(activity, intent, callback);
+//            }
+            innerFragmentForResult(activity, intent, callback);
         }
     }
 
@@ -29,14 +29,14 @@ public class ActivityStarter {
         startActivityForResult(fragment.getActivity(), intent, callback);
     }
 
-    private static void innerSupportFragmentForResult(FragmentActivity activity, Intent intent, Callback callback) {
-        InnerSupportFragment fragment = (InnerSupportFragment) activity.getSupportFragmentManager().findFragmentByTag(TAG);
-        if (fragment == null) {
-            fragment = new InnerSupportFragment();
-            activity.getSupportFragmentManager().beginTransaction().add(fragment, TAG).commit();
-        }
-        fragment.startActivityForResult(intent, callback);
-    }
+//    private static void innerSupportFragmentForResult(android.support.v4.app.FragmentActivity activity, Intent intent, Callback callback) {
+//        InnerSupportFragment fragment = (InnerSupportFragment) activity.getSupportFragmentManager().findFragmentByTag(TAG);
+//        if (fragment == null) {
+//            fragment = new InnerSupportFragment();
+//            activity.getSupportFragmentManager().beginTransaction().add(fragment, TAG).commit();
+//        }
+//        fragment.startActivityForResult(intent, callback);
+//    }
 
     private static void innerFragmentForResult(Activity activity, Intent intent, Callback callback) {
         InnerFragment fragment = (InnerFragment) activity.getFragmentManager().findFragmentByTag(TAG);
@@ -47,25 +47,25 @@ public class ActivityStarter {
         fragment.startActivityForResult(intent, callback);
     }
 
-    public static class InnerSupportFragment extends android.support.v4.app.Fragment {
-        private int requestCode = 0;
-        private SparseArray<Callback> callbackSparseArray = new SparseArray<>();
-
-        public synchronized void startActivityForResult(Intent intent, Callback callback) {
-            int realRequestCode = requestCode++;
-            callbackSparseArray.put(realRequestCode, callback);
-            super.startActivityForResult(intent, realRequestCode);
-        }
-
-        @Override
-        public void onActivityResult(int requestCode, int resultCode, Intent data) {
-            Callback callback = callbackSparseArray.get(requestCode);
-            callbackSparseArray.remove(requestCode);
-            if (callback != null) {
-                callback.onActivityResult(resultCode, data);
-            }
-        }
-    }
+//    public static class InnerSupportFragment extends android.support.v4.app.Fragment {
+//        private int requestCode = 0;
+//        private SparseArray<Callback> callbackSparseArray = new SparseArray<>();
+//
+//        public synchronized void startActivityForResult(Intent intent, Callback callback) {
+//            int realRequestCode = requestCode++;
+//            callbackSparseArray.put(realRequestCode, callback);
+//            super.startActivityForResult(intent, realRequestCode);
+//        }
+//
+//        @Override
+//        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//            Callback callback = callbackSparseArray.get(requestCode);
+//            callbackSparseArray.remove(requestCode);
+//            if (callback != null) {
+//                callback.onActivityResult(resultCode, data);
+//            }
+//        }
+//    }
 
     public static class InnerFragment extends android.app.Fragment {
         private int requestCode = 0;
