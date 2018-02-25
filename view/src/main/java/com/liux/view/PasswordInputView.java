9d260a61c7d84a0b22e13f29ee7ced8aff7726df
Liux <lx0758@qq.com>
2018-02-25 18:45:30 +0800

    添加三个自定义控件

--- view/src/main/java/com/liux/view/PasswordInputView.java
+++ view/src/main/java/com/liux/view/PasswordInputView.java
@@ -4,59 +4,49 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.text.InputFilter;
 import android.text.method.MovementMethod;
 import android.util.AttributeSet;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.widget.EditText;
+
+import java.util.Arrays;
 
 
 @SuppressLint("DrawAllocation")
 public class PasswordInputView extends EditText {
     
-    private int mPasswordLength = 6;
-    private int mPasswordColor = 0xff000000;
-    private int mPasswordWidth = 10;
-    private int mPasswordInterval = 10;
-    private Drawable mPasswordDrawable = null;
+    private int mLength = 6;
+    private int mColor = 0xff000000;
+    private int mWidth = 10;
+    private int mInterval = 10;
+    private Drawable mDrawable = null;
 
     private int mOneWidth;
-    private int mTextLength;
     private Paint mPasswordPaint;
 
     private OnPasswordListener mOnPasswordListener;
 
     public PasswordInputView(Context context) {
         super(context);
+        initView(null);
     }
     
-    public PasswordInputView(Context context, AttributeSet attr) {
-        super(context, attr);
-        init(context, attr);
+    public PasswordInputView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(attrs);
     }
-    
-    private void init(Context context, AttributeSet attr) {
-        TypedArray ta = context.obtainStyledAttributes(attr, R.styleable.PasswordInputView);
-        try {
-            mPasswordWidth = ta.getDimensionPixelSize(R.styleable.PasswordInputView_password_size, dp2px(mPasswordWidth));
-            mPasswordColor = ta.getColor(R.styleable.PasswordInputView_password_color, mPasswordColor);
-            mPasswordLength = ta.getInt(R.styleable.PasswordInputView_password_length, mPasswordLength);
-            mPasswordDrawable = ta.getDrawable(R.styleable.PasswordInputView_password_drawable);
-            mPasswordInterval = ta.getDimensionPixelSize(R.styleable.PasswordInputView_password_interval, dp2px(mPasswordInterval));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        ta.recycle();
 
-        setCursorVisible(false);
-        setFilters(new InputFilter[] {new InputFilter.LengthFilter(mPasswordLength)});
-        
-        mPasswordPaint = new Paint();
-        mPasswordPaint.setAntiAlias(true);
-        mPasswordPaint.setColor(mPasswordColor);
-        mPasswordPaint.setStrokeWidth(mPasswordWidth);
+    public PasswordInputView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(attrs);
     }
 
     @Override
@@ -70,7 +60,7 @@ public class PasswordInputView extends EditText {
         setMeasuredDimension(getDefaultSize(MeasureSpec.UNSPECIFIED, widthMeasureSpec), getDefaultSize(MeasureSpec.UNSPECIFIED, heightMeasureSpec));
 
         int width = getMeasuredWidth();
-        int height = (width - ((mPasswordLength - 1) * mPasswordInterval)) / mPasswordLength;
+        int height = (width - ((mLength - 1) * mInterval)) / mLength;
         mOneWidth = height;
         widthMeasureSpec = MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY);
         heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
@@ -79,52 +69,57 @@ public class PasswordInputView extends EditText {
     }
 
     @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-    }
-
-    @Override
     protected void onDraw(Canvas canvas) {
         //super.onDraw(canvas);
         int width = getWidth();
         int height = getHeight();
+        int length = length();
 
         int left = 0;
-        for (int i = 0; i < mPasswordLength; i++) {
+        for (int i = 0; i < mLength; i++) {
             RectF rect = new RectF(left, 0, left + mOneWidth, height);
 
             // 画边框
-            mPasswordDrawable.setBounds((int) rect.left, (int) rect.top, (int) rect.right, (int) rect.bottom);
-            mPasswordDrawable.draw(canvas);
-
-            if (i < mTextLength) {
-                canvas.drawCircle(rect.left + rect.width() / 2, rect.top + rect.height() / 2, mPasswordWidth, mPasswordPaint);
+            if (mDrawable != null) {
+                mDrawable.setBounds((int) rect.left, (int) rect.top, (int) rect.right, (int) rect.bottom);
+                mDrawable.draw(canvas);
             }
 
-            left += mOneWidth + mPasswordInterval;
+            // 画圆圈
+            if (i < length) {
+                canvas.drawCircle(rect.left + rect.width() / 2, rect.top + rect.height() / 2, mWidth, mPasswordPaint);
+            }
+
+            left += mOneWidth + mInterval;
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean result = super.onTouchEvent(event);
+        setSelection(getText().length());
+        return result;
     }
     
     @Override
     protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
         super.onTextChanged(text, start, lengthBefore, lengthAfter);
 
-        mTextLength = text.length();
         invalidate();
 
         if (mOnPasswordListener != null) {
-            mOnPasswordListener.setOnPasswordChange(text);
+            mOnPasswordListener.onPasswordChange(text);
         }
 
-        if (text.length() >= mPasswordLength) {
+        if (text.length() >= mLength) {
             if (mOnPasswordListener != null) {
-                mOnPasswordListener.setOnPasswordFinished(text.subSequence(0, mPasswordLength));
+                mOnPasswordListener.onPasswordFinished(text.subSequence(0, mLength));
             }
         }
     }
 
-    public void setOnPasswordListener(OnPasswordListener mOnPasswordListener) {
-        this.mOnPasswordListener = mOnPasswordListener;
+    public void setOnPasswordListener(OnPasswordListener listener) {
+        this.mOnPasswordListener = listener;
     }
 
     /**
@@ -137,10 +132,70 @@ public class PasswordInputView extends EditText {
         return (int) (dpValue * scale + 0.5f);
     }
 
+    private void initView(AttributeSet attrs) {
+        if (attrs != null) {
+            mLength = attrs.getAttributeIntValue(android.R.attr.maxLength, mLength);
+
+            TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.PasswordInputView);
+
+            mWidth = ta.getDimensionPixelSize(R.styleable.PasswordInputView_piv_size, dp2px(mWidth));
+            mColor = ta.getColor(R.styleable.PasswordInputView_piv_color, mColor);
+            mDrawable = ta.getDrawable(R.styleable.PasswordInputView_piv_drawable);
+            mInterval = ta.getDimensionPixelSize(R.styleable.PasswordInputView_piv_interval, dp2px(mInterval));
+
+            ta.recycle();
+        }
+
+        if (mLength < 1) mLength = 6;
+        if (mInterval < 0) mInterval = 0;
+
+        setBackgroundColor(Color.TRANSPARENT);
+        setMaxLength(mLength);
+        setShieldSelector();
+
+        mPasswordPaint = new Paint();
+        mPasswordPaint.setAntiAlias(true);
+        mPasswordPaint.setColor(mColor);
+        mPasswordPaint.setStrokeWidth(mWidth);
+    }
+
+    private void setMaxLength(int maxLength) {
+        InputFilter[] oldInputFilters = getFilters();
+        InputFilter[] newInputFilters = Arrays.copyOf(oldInputFilters, oldInputFilters.length + 1);
+        newInputFilters[oldInputFilters.length] = new InputFilter.LengthFilter(maxLength);
+        setFilters(newInputFilters);
+    }
+
+    private void setShieldSelector() {
+        setLongClickable(false);
+        setCursorVisible(false);
+        setCustomSelectionActionModeCallback(new ActionMode.Callback(){
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+
+            }
+        });
+    }
+
     public interface OnPasswordListener {
 
-        void setOnPasswordChange(CharSequence charSequence);
+        void onPasswordChange(CharSequence charSequence);
 
-        void setOnPasswordFinished(CharSequence charSequence);
+        void onPasswordFinished(CharSequence charSequence);
     }
 }
